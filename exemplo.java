cfa7a3cd787753de76538824a9aca63b241e7000
lmmLucasMachado <lmlucasmachadom@gmail.com>
2019-10-31 23:28:49 -0300

    other helloword diferent

---
+++ exemplo.java
@@ -0,0 +1,15 @@
+
+public class exemplo{
+
+    public static void mostrarMensagem() {
+        System.out.println("Hello word diferente");
+        System.out.println("É necessario a resposta");
+        System.out.println("23");
+
+    }
+    
+    public static void main(String[] args) {
+        mostrarMensagem();
+    }
+
+}
