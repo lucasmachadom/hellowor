e62c74a477054b0833cae4cee4dec28119ed7fdf
lmmLucasMachado <lmlucasmachadom@gmail.com>
2020-10-23 23:26:00 -0300

    Implementation sum

---
+++ operation/main.java
@@ -0,0 +1,15 @@
+
+public class main{
+
+    public static void main(String[] args) {
+        Operations op = new Operations();
+
+        op.setNum1 = 35.5;
+        op.setNum2 = 6.5;
+        
+        System.out.println( op.getResultSum()) ;
+    }
+
+}
+
+
